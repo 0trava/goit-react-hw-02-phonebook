243e0d74697d1804faec59dbe83c20d9ccd7c07e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-04-18 18:34:45 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,22 +10,22 @@ import ContactsList from "./ContactsList/ContactsList";
 import Filter from "./Filter/Filter";
 import css from "./App.module.css";
 
-// state = {
-//   contacts: [
-//     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-//     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-//     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-//     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-//   ],
-//   filter: '',
-//   name: '',
-//   number: ''
-// }
-
-
 
 
 class App extends Component {
+
+
+state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+  name: '',
+  number: ''
+}
 
 
 
@@ -35,14 +35,13 @@ class App extends Component {
 
       return (
       <div className={css.container}>
-        <h1>Phonebook</h1>
+        <h1 className={css.section_title}>Phonebook</h1>
         <ContactForm />
 
-        <h2>Contacts</h2>
+        <h2 className={css.section_title}>Contacts</h2>
         <Filter />
         <ContactsList></ContactsList>
       </div>
-
   );}
 };
 
